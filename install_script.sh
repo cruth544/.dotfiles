5ede72af3f01f3a1b931ad7556875ad84961be60
Chad Rutherford <chad8708@c02rf10sg8wm.attlocal.net>
2017-01-23 11:47:37 -0800

    started working on install script

--- install_script.sh
+++ install_script.sh
@@ -1,1 +1,8 @@
-https://alicoding.com/how-to-start-mongodb-automatically-when-starting-your-mac-os-x/
+#!/bin/bash
+# https://alicoding.com/how-to-start-mongodb-automatically-when-starting-your-mac-os-x/
+echo "brew update"
+echo "brew tap caskroom/cask"
+echo "brew cask install google-chrome"
+echo "brew cask install slack"
+
+echo "brew linkapps"

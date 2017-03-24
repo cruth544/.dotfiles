32f0ece5da85db7238d18558d126c31c14faab24
Chad Rutherford <chad8708@c02rf10sg8wm.attlocal.net>
2017-03-24 14:21:04 -0700

    edit install script

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# use this for always having mongo running
 # https://alicoding.com/how-to-start-mongodb-automatically-when-starting-your-mac-os-x/
-echo "brew update"
-echo "brew tap caskroom/cask"
-echo "brew cask install google-chrome"
-echo "brew cask install slack"
 
-echo "brew linkapps"
+echo "cp ~/.dotfiles/.bash_profile ~/"
+echo "source ~/.bash_profile"
+
+# in development app installs
+#echo "brew update"
+#echo "brew tap caskroom/cask"
+#echo "brew cask install google-chrome"
+#echo "brew cask install slack"
+
+#echo "brew linkapps"
